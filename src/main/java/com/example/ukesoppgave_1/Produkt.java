c8d5bdf1c51e434583681f559ffcb98d6c02104d
vanmelon <hms2006@online.no>
2023-12-10 15:37:40 +0100

    legger til java og lit css

--- src/main/java/com/example/ukesoppgave_1/Produkt.java
+++ src/main/java/com/example/ukesoppgave_1/Produkt.java
@@ -1,4 +1,14 @@
 package com.example.ukesoppgave_1;
 
 public class Produkt {
+    private String navn;
+    private double pris;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public double getPris() {
+        return pris;
+    }
 }
--- src/main/java/com/example/ukesoppgave_1/ProduktController.java
+++ src/main/java/com/example/ukesoppgave_1/ProduktController.java
@@ -1,4 +1,16 @@
 package com.example.ukesoppgave_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProduktController {
+    private List<Produkt> produkter = new ArrayList<>();
+
+    public void leggTilProdukt(Produkt produkt) {
+        produkter.add(produkt);
+    }
+
+    public List<Produkt> hentAlleProdukter() {
+        return produkter;
+    }
 }

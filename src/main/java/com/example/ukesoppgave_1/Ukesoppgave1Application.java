91d52bd77b4c3d8b058a80d8b657a20e0f1cbb5a
vanmelon <hms2006@online.no>
2023-12-10 15:25:16 +0100

    laget reposetory
    laget reposetory

---
+++ src/main/java/com/example/ukesoppgave_1/Ukesoppgave1Application.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgave_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ukesoppgave1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ukesoppgave1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukesoppgave_1/Ukesoppgave1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgave_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ukesoppgave1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
